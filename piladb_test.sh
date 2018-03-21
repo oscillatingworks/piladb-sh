70cd29f00779dbdcd03f8d872434829970db7e72
Fernando Alvarez <info@fer.ac>
2018-03-21 14:17:47 +0100

    Improve exit status when failing

--- piladb_test.sh
+++ piladb_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -eo pipefail
 
 source piladb.sh
 _require jq || exit 1
@@ -22,7 +22,7 @@ ok=$(piladb_status | jq '.status')
 if [ "$ok" != '"OK"' ]; then
    _log "ERROR: status is $ok, expected OK"
    piladb_stop
-   exit
+   exit 1
 fi
 
 pid=$(piladb_status | jq '.pid')
@@ -35,7 +35,7 @@ max_stack_size=$(piladb_config_get MAX_STACK_SIZE | jq '.element')
 if [ "$max_stack_size" != "-1" ]; then
    _log "ERROR: MAX_STACK_SIZE is $max_stack_size, expected -1"
    piladb_stop
-   exit
+   exit 1
 fi
 
 piladb_config_set MAX_STACK_SIZE 10
@@ -54,7 +54,7 @@ number_of_databases=$(piladb_show_databases | jq '.number_of_databases')
 if [ "$number_of_databases" -ne "0" ]; then
    _log "ERROR: no databases expected, got $number_of_databases"
    piladb_stop $pid
-   exit
+   exit 1
 fi
 
 piladb_create_database db1
@@ -65,7 +65,7 @@ number_of_databases=$(piladb_show_databases | jq '.number_of_databases')
 if [ "$number_of_databases" -ne "3" ]; then
    _log "ERROR: number of databases is $number_of_databases, expected 3"
    piladb_stop $pid
-   exit
+   exit 1
 fi
 
 piladb_delete_database db2
@@ -73,7 +73,7 @@ number_of_databases=$(piladb_show_database db2 | jq '.number_of_databases')
 if [ -n "$number_of_databases" ]; then
    _log "ERROR: database db2 exists"
    piladb_stop $pid
-   exit
+   exit 1
 fi
 
 
@@ -87,7 +87,7 @@ number_of_stacks=$(piladb_show_database db1 | jq '.number_of_stacks')
 if [ "$number_of_stacks" -ne "3" ]; then
    _log "ERROR: number of stacks is $number_of_stacks, expected 3"
    piladb_stop $pid
-   exit
+   exit 1
 fi
 
 piladb_show_stack db1 mystack1
@@ -100,21 +100,21 @@ popped=$(piladb_POP db1 mystack1 | jq '.element')
 if [ "$popped" != '"3"' ]; then
    _log "ERROR: popped is $popped, expected 3"
    piladb_stop $pid
-   exit
+   exit 1
 fi
 
 peek=$(piladb_PEEK db1 mystack1 | jq '.element')
 if [ "$peek" != '"2"' ]; then
    _log "ERROR: peek is $peek, expected 2"
    piladb_stop $pid
-   exit
+   exit 1
 fi
 
 size=$(piladb_SIZE db1 mystack1)
 if [ "$size" -ne "2" ]; then
    _log "ERROR: size is $size, expected 2"
    piladb_stop $pid
-   exit
+   exit 1
 fi
 
 piladb_FLUSH db1 mystack1
@@ -123,7 +123,7 @@ size=$(piladb_SIZE db1 mystack1)
 if [ "$size" -ne "0" ]; then
    _log "ERROR: size is $size, expected 0"
    piladb_stop $pid
-   exit
+   exit 1
 fi
 
 piladb_delete_stack db1 mystack2
@@ -132,7 +132,7 @@ number_of_stacks=$(piladb_show_database db1 | jq '.number_of_stacks')
 if [ "$number_of_stacks" -ne "2" ]; then
    _log "ERROR: number of stacks is $number_of_stacks, expected 2"
    piladb_stop $pid
-   exit
+   exit 1
 fi
 
 
